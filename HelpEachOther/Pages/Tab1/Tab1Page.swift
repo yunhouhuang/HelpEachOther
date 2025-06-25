7faa0ef40b7cbc4c9626daf9cc409edfa8c046ad
Yunhou Huang <54256836+yunhouhuang@users.noreply.github.com>
2025-06-25 20:23:00 +0800

    新增首页 Header

--- HelpEachOther/Pages/Tab1/Tab1Page.swift
+++ HelpEachOther/Pages/Tab1/Tab1Page.swift
@@ -10,7 +10,17 @@ import SwiftUI
 struct Tab1Page: View {
     var body: some View {
         VStack{
-            
+            VStack{
+                HStack(alignment: .bottom){
+                    Text("Today").font(Font.system(size: 32).bold())
+                    Text("9月29日").font(Font.system(size: 24).bold()).foregroundStyle(.secondary)
+                    Spacer()
+                    Image("avatar").resizable().frame(width: 42, height: 42).clipShape(Circle())
+                }.padding(.horizontal)
+            }
+            ScrollView(){
+                
+            }
             
             
         }
